26ebee5d1c723a2434bb14c4eedd89b854ab8ed8
unknown <soligalskideleo@gmail.com>
2023-03-30 16:39:02 +0530

    Added Header and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Header from "./components/Header"
 import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <div>App</div>
+    </>
+   
   )
 }
 
